5f7ef8d16bb09a2c2364cc6d12115b183feb99a4
TangoBee <71539314+TangoBeee@users.noreply.github.com>
2022-09-08 16:18:24 +0530

    con is null has been "fixed"

--- src/main/java/me/tangobee/pepebank/Database/DBSQL.java
+++ src/main/java/me/tangobee/pepebank/Database/DBSQL.java
@@ -4,10 +4,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DBSQL {
-    static final String driver = "com.mysql.cj.jdbc.Driver";
-    static final String url = "jdbc:mysql://localhost:3306/";
-    static final String username = "root";
-    static final String pass = "yourpass";
     public static void main(String[] args) {
         Connection connection = getConnection();
         assert connection != null;
@@ -17,16 +13,20 @@ public class DBSQL {
     public static Connection getConnection() {
 
         try{
+            String driver = "com.mysql.cj.jdbc.Driver";
+            String url = "jdbc:mysql://localhost:3306/";
+            String username = "root";
+            String pass = "yourpass";
             Class.forName(driver);
 
             Connection conn;
 
             conn = DriverManager.getConnection(url,username,pass);
 
-            conn.prepareStatement("CREATE DATABASE IF NOT EXISTS banking_system;").executeQuery();
-            conn.prepareStatement("USE banking_system").executeQuery();
-            conn.prepareStatement("CREATE TABLE IF NOT EXISTS user_info(`name` varchar(20) NOT NULL, `email` varchar(100) UNIQUE NOT NULL, `dob` DATE NOT NULL, `age` TINYINT unsigned NOT NULL, `gender` varchar(6) NOT NULL, `phoneno` varchar(20) NOT NULL, `address` text NOT NULL, `governid` varchar(20), `networth` int NOT NULL, `acctype` varchar(8) NOT NULL, `accountno` varchar(100) UNIQUE NOT NULL, `password` varchar(50) NOT NULL);").executeQuery();
-            conn.prepareStatement("CREATE TABLE IF NOT EXISTS banking(transaction INT DEFAULT 0, balance INT DEFAULT 500, email varchar(100) UNIQUE NOT NULL);").executeQuery();
+            conn.prepareStatement("CREATE DATABASE IF NOT EXISTS banking_system;").executeUpdate();
+            conn.prepareStatement("USE banking_system").executeUpdate();
+            conn.prepareStatement("CREATE TABLE IF NOT EXISTS user_info(`name` varchar(20) NOT NULL, `email` varchar(100) UNIQUE NOT NULL, `dob` DATE NOT NULL, `age` TINYINT unsigned NOT NULL, `gender` varchar(6) NOT NULL, `phoneno` varchar(20) NOT NULL, `address` text NOT NULL, `governid` varchar(20), `networth` int NOT NULL, `acctype` varchar(8) NOT NULL, `accountno` varchar(100) UNIQUE NOT NULL, `password` varchar(50) NOT NULL);").executeUpdate();
+            conn.prepareStatement("CREATE TABLE IF NOT EXISTS banking(transaction INT DEFAULT 0, balance INT DEFAULT 500, email varchar(100) UNIQUE NOT NULL);").executeUpdate();
 
             return conn;
         }
